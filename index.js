865a0f54aa8d816bf131581641af91573ba77967
Joshua Kon <jkon@flatironssolutions.com>
2015-09-21 11:22:23 -0600

    added some extra error handling around reading files updated help read file function param to use __dirname to get the correct context from anywhere refactored common flags to be read inside a helper function that is called after the command is determined so that the no secret provided message doesn't come up before help if no arguments are provided

--- index.js
+++ index.js
@@ -20,39 +20,56 @@ if (process.argv.indexOf('decode') >= 0) {
 
 if (_.intersection(process.argv, ['help', '-h', '-help', '--h', '--help']).length > 0) {
   if (command) {
-    console.log(fs.readFileSync(command + '_help.txt').toString());
+    console.log(fs.readFileSync(__dirname + '/' + command + '_help.txt').toString());
     process.exit(0);
   } else {
-    console.log(fs.readFileSync('help.txt').toString());
+    console.log(fs.readFileSync(__dirname + '/help.txt').toString());
     process.exit(0);
   }
 }
 
-var indexOfSecretFlag = process.argv.indexOf('-s');
-var indexOfSecretFileFlag = process.argv.indexOf('-sf');
-var indexOfAlgFlag = process.argv.indexOf('-a');
+
 var payload;
-//console.log('index of secret flag: ' + indexOfSecretFlag);
-//console.log('index of alg flag: ' + indexOfAlgFlag);
-var secret;
-//check for a secret
-if (indexOfSecretFlag < 0) {
-  if (indexOfSecretFileFlag < 0) {
-    console.error('Must provide a secret to sign or verify the jwt with');
-    process.exit(0);
-  } else {
-    //set the secret from a file
-    secret = fs.readFileSync(process.argv[indexOfSecretFileFlag + 1]);
-    console.log(secret);
-  }
-} else {
-  //set the secret from the argument
-  secret = process.argv[indexOfSecretFlag + 1];
-}
-//set algorithm. HS256 is default
-var alg = indexOfAlgFlag >= 0 ? process.argv[indexOfAlgFlag + 1] : 'HS256';
 
+var readCommonFlags = function () {
+  var flagValues = {};
+
+  var indexOfSecretFlag = process.argv.indexOf('-s');
+  var indexOfSecretFileFlag = process.argv.indexOf('-sf');
+  var indexOfAlgFlag = process.argv.indexOf('-a');
+  //console.log('index of secret flag: ' + indexOfSecretFlag);
+  //console.log('index of alg flag: ' + indexOfAlgFlag);
+  //check for a secret
+  var fileName;
+  if (indexOfSecretFlag < 0) {
+    if (indexOfSecretFileFlag < 0) {
+      console.error('Must provide a secret to sign or verify the jwt with');
+      process.exit(0);
+    } else {
+      //set the secret from a file
+      fileName = process.argv[indexOfSecretFileFlag + 1];
+      try {
+        flagValues.secret = fs.readFileSync(fileName);
+        console.log(secret);
+      } catch (err) {
+        console.log('No such file: ' + fileName);
+        process.exit(0);
+      }
+    }
+  } else {
+    //set the secret from the argument
+    flagValues.secret = process.argv[indexOfSecretFlag + 1];
+  }
+//set algorithm. HS256 is default
+  flagValues.alg = indexOfAlgFlag >= 0 ? process.argv[indexOfAlgFlag + 1] : 'HS256';
+
+
+  return flagValues;
+};
+
+var commonValues;
 if (command === 'decode') {
+  commonValues = readCommonFlags();
   //decoding a jwt
   //check to if a token was supplied
   var indexOfTokenFlag = process.argv.indexOf('-t');
@@ -63,13 +80,14 @@ if (command === 'decode') {
   } else {
     //token was supplied. verify token with secret provided using algorithm provided
     var token = process.argv[indexOfTokenFlag + 1];
-    payload = jwt.verify(token, secret, {algorithm: alg});
+    payload = jwt.verify(token, commonValues.secret, {algorithm: commonValues.alg});
 
     console.log('Payload:  ');
     console.log(payload);
   }
 
 } else if (command === 'encode') {
+  commonValues = readCommonFlags();
   //encoding a jwt
   var indexOfFileFlag = process.argv.indexOf('-f');
   var indexOfExpFlag = process.argv.indexOf('-e');
@@ -82,10 +100,14 @@ if (command === 'decode') {
     process.exit(0);
   }
 
-  var filename = process.argv[indexOfFileFlag + 1];
-  var file = fs.readFileSync(filename);
-  payload = JSON.parse(file.toString());
-
+  var fileName = process.argv[indexOfFileFlag + 1];
+  try {
+    var file = fs.readFileSync(fileName);
+    payload = JSON.parse(file.toString());
+  } catch (err) {
+    console.log('No such file: ' + fileName);
+    process.exit(0);
+  }
 
   if (indexOfExpFlag >= 0) {
     var expValue = process.argv[indexOfExpFlag + 1];
@@ -99,12 +121,12 @@ if (command === 'decode') {
   console.log('Signing jwt with payload: ');
   console.log(payload);
 
-  var signedJWT = jwt.sign(payload, secret, {algorithm: alg});
+  var signedJWT = jwt.sign(payload, commonValues.secret, {algorithm: commonValues.alg});
   console.log(signedJWT);
   copyPaste.copy(signedJWT);
 
 } else {
-  console.log(fs.readFileSync('help.txt').toString());
+  console.log(fs.readFileSync(__dirname + '/help.txt').toString());
   process.exit(0);
 }
 
